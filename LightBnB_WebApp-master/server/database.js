f9b957eed5a0a7473bfcc943a24f1a96f689dd61
Calvin Ho <calvin.ch.ho@gmail.com>
2022-06-30 21:23:35 -0400

    Add db route for lightbnb reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -105,9 +105,31 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// }
+
+const getAllReservations = (guest_id, limit = 10) => {
+
+  return pool
+    .query(`SELECT properties.*
+    FROM properties
+    JOIN reservations ON reservations.property_id = properties.id
+    JOIN property_reviews ON property_reviews.property_id = properties.id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2`, [`${guest_id}`, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      return err.message;
+    });
+};
+
+
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
